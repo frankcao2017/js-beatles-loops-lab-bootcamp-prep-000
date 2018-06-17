684393d4b8ab73a656855261ab19d3a854887339
frankcao@me.com Cao <frankcao@me.com>
2018-06-17 09:37:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ function theBeatlesPlay(musicians, instruments) {
   return arr
 }
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
 
 function johnLennonFacts(facts) {
   var theFacts = []
